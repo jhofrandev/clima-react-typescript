f455adfffbc91af79449ae821921267e443acc24
Jhofran Briceño <jhofran.dev@outlook.com>
2025-05-29 16:15:36 -0500

    feat: enhance weather app with loading state, error handling, and weather details display

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import styles from "./App.module.css";
+import Alert from "./components/Alert/Alert";
 import Form from "./components/Form/Form";
+import Spinner from "./components/Spinner/Spinner";
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail";
 import useWeather from "./hooks/useWeather";
 
 function App() {
-  const { fetchWeather } = useWeather();
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } =
+    useWeather();
 
   return (
     <>
@@ -11,7 +15,9 @@ function App() {
 
       <div className={styles.container}>
         <Form fetchWeather={fetchWeather} />
-        <p>2</p>
+        {loading && <Spinner />}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+        {notFound && <Alert>Ciudad no encontrada</Alert>}
       </div>
     </>
   );
